62e589b24ed66f770341693999d429b3a6a1d6fb
Shahid N. Shah <shahid@shah.org>
2024-05-16 10:30:20 -0400

    fix: GitHub pages needs base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tech-by-design.github.io',
+	base: 'docs.techbd.org',
 	integrations: [
 		starlight({
 			title: 'TechBD Docs',
