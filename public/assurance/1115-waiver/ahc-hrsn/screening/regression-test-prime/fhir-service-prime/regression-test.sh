00173c34425ed297c10a16736fb18b103b3b448e
Shahid N. Shah <shahid@shah.org>
2024-05-16 12:42:27 -0400

    fix: use relative paths in Git

--- public/assurance/1115-waiver/ahc-hrsn/screening/regression-test-prime/fhir-service-prime/regression-test.sh
+++ public/assurance/1115-waiver/ahc-hrsn/screening/regression-test-prime/fhir-service-prime/regression-test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # requires NodeJS, httypyac, pip, Python, and junit2htmlreport
 # npm install -g httpyac
@@ -9,20 +11,22 @@ SRC_DIR=$(readlink -f $SCRIPT_DIR/../../../../../../../assurance/1115-waiver/ahc
 TIMESTAMP=$(date +'%Y/%m/%Y-%m-%d-%H-%M-%S')
 RESULTS_DIR=$SCRIPT_DIR/results
 LATEST_RESULTS_DIR=$RESULTS_DIR/$TIMESTAMP
-LATEST_RESULTS_SYMLINK=$RESULTS_DIR/latest
 
 mkdir -p $LATEST_RESULTS_DIR
 
-echo $SCRIPT_DIR
-echo $SRC_DIR
-echo $TIMESTAMP
-echo $RESULTS_DIR
-echo $LATEST_RESULTS_DIR
-echo $LATEST_RESULTS_SYMLINK
+# echo $SCRIPT_DIR
+# echo $SRC_DIR
+# echo $TIMESTAMP
+# echo $RESULTS_DIR
+# echo $LATEST_RESULTS_DIR
 
 httpyac $SRC_DIR/fhir-service.test.http --all > $LATEST_RESULTS_DIR/regression-test-results.httpyac.txt
 httpyac $SRC_DIR/fhir-service.test.http --all --junit > $LATEST_RESULTS_DIR/regression-test-results.httpyac.junit.xml
 python -m junit2htmlreport $LATEST_RESULTS_DIR/regression-test-results.httpyac.junit.xml $LATEST_RESULTS_DIR/regression-test-results.httpyac.junit.xml.html
 
-rm -f $LATEST_RESULTS_SYMLINK
-ln -s $LATEST_RESULTS_DIR $LATEST_RESULTS_SYMLINK
+# make sure symlinks are relative (for Git)
+cd $RESULTS_DIR
+rm -f latest
+ln -s $TIMESTAMP latest
+
+echo $(ls -al latest)
